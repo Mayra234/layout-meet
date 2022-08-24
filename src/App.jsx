6a15379a8e12f933313363ccfaee03bd6c38c8cb
Mayra234 <alejandradiavanera@gmail.com>
2022-08-23 19:55:57 -0500

    Se arregla el archivo de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { MainLayout } from './layouts/MainLAyout';
+import { Screen } from './pages/Screen';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />} />
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Screen />
+            </MainLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
