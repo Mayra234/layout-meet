3c92c3a9127a9cde24fd00a80b0770e3b9ca07fa
Mayra234 <alejandradiavanera@gmail.com>
2022-08-23 19:50:49 -0500

    Se hacen arreglos en el componente botón

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import { ReactDOM } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './index.css';
 
-export const Button = ({ icon = '', className = '' }) => {
+export const Button = ({
+  icon = '',
+  className = '',
+  palette = 'secondary',
+  type = 'button',
+  onClick = () => {},
+  ...props
+}) => {
+  const [style, setStyle] = useState(props.style);
+
+  const changePalette = () => {
+    if (palette === 'primary') {
+      setStyle((prevStyle) => ({ ...prevStyle, background: '#3c4043' }));
+    } else if (palette === 'secondary') {
+      setStyle((prevStyle) => ({ ...prevStyle, background: '#e84332' }));
+    }
+  };
+
+  useEffect(() => {
+    changePalette();
+  }, [palette]);
+
   return (
-    <button type="button" className={'btn ' + className}>
+    <button
+      style={style}
+      onClick={onClick}
+      palette={palette}
+      type={type}
+      className={'btn ' + className}
+    >
       <FontAwesomeIcon icon={icon} />
     </button>
   );
